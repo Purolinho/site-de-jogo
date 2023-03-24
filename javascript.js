67cf4cf21cd82a5138da175407d07ba15a65b865
Linho <106666571+Purolinho@users.noreply.github.com>
2023-03-24 12:10:11 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,59 @@
+const titulo = document.getElementById("titulo")
+const descrição = document.getElementById("descrição")
+const avaliação = document.getElementById("avaliação")
+const link = document.getElementById("link")
+const info = document.getElementById("info")
+
+const img1 = window.document.getElementById("img1").addEventListener("mouseenter", () => {
+    titulo.innerHTML = "Ori and the Will of Wisps"
+    descrição.innerHTML = "The voice of the Spirit Tree in the forest of Nibel narrates the story of when Ori, a guardian spirit, fell from it during a storm as a newborn and was adopted by a creature named Naru, who raised Ori as her own. A cataclysmic event soon makes all of the forest wither, and Naru dies of starvation."
+    avaliação.innerHTML = "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    link.href = "https://store.steampowered.com/app/1057090/Ori_and_the_Will_of_the_Wisps/"
+    link.innerHTML = "Ori and the Will of Wisps"
+    info.style.display = "flex"
+
+
+    this.img1.style.flexGrow = "1"
+
+})
+
+
+const img2 = window.document.getElementById("img2").addEventListener("mouseenter", () => {
+    titulo.innerHTML = "Celeste"
+    descrição.innerHTML = "A young woman undertakes the challenge of climbing a mountain with dangerous perils and mysterious powers. Throughout her journey, she is confronted by Part Of Her, her bad side doppelgänger."
+    avaliação.innerHTML = "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    link.href = "https://store.steampowered.com/app/504230/Celeste/"
+    link.innerHTML = "Celeste"
+    info.style.display = "flex"
+
+
+})
+
+const img3 = window.document.getElementById("img3").addEventListener("mouseenter", () => {
+    titulo.innerHTML = "Five Nights at Freddy's"
+    descrição.innerHTML = "As an anonymous night guard, you must survive five nights as you are hunted by five animatronics hell-bent on killing you. Freddy Fazbear's Pizzeria is a fantastic place for children and adults can have fun with all the robotic animals; Freddy, Bonnie, Chica, and Foxy."
+    avaliação.innerHTML = "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    link.href = "https://store.steampowered.com/app/319510/Five_Nights_at_Freddys/"
+    link.innerHTML = "Five Nights at Freddy's"
+    info.style.display = "flex"
+})
+
+const img4 = window.document.getElementById("img4").addEventListener("mouseenter", () => {
+    titulo.innerHTML = "Minecraft"
+    descrição.innerHTML = "In Minecraft, players explore a blocky, procedurally generated, three-dimensional world with virtually infinite terrain and may discover and extract raw materials, craft tools and items, and build structures, earthworks, and machines."
+    avaliação.innerHTML = "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    link.href = "https://www.minecraft.net/pt-br"
+    link.innerHTML = "Minecraft"
+    info.style.display = "flex"
+})
+
+
+
+const img5 = window.document.getElementById("img5").addEventListener("mouseenter", () => {
+    titulo.innerHTML = "GTA 5"
+    descrição.innerHTML = "When a young street hustler, a retired bank robber, and a terrifying psychopath find themselves entangled with some of the most frightening and deranged elements of the criminal underworld, the U.S. Government, and the entertainment industry, they must pull off a series of dangerous heists to survive in a ruthless city"
+    avaliação.innerHTML = "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    link.href = "https://store.steampowered.com/app/271590/Grand_Theft_Auto_V/"
+    link.innerHTML = "GTA 5"
+    info.style.display = "flex"
+})
